055dba36b811da0599dbc7639ca426d39aab384f
AmmarLeo <96083359+AmmarLeo@users.noreply.github.com>
2022-05-18 17:53:28 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
-const colorsList = [
-  "#ff7373",
-  "#008080",
-  "#666666",
-  "#468499",
-  "#333333",
-  "#101010",
-  "#00ff7f",
-];
+const hexs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
 const colorPTag = document.getElementById("color");
 const colorBtn = document.getElementById("color-btn");
 const divContainer = document.getElementById("container");
 
 colorBtn.addEventListener("click", function () {
-  const randomNumber = Math.floor(Math.random() * colorsList.length);
-  document.querySelector("body").style.background = colorsList[randomNumber];
-  colorPTag.innerHTML = colorsList[randomNumber];
-
-  if (
-    colorsList[randomNumber] === "#666666" ||
-    colorsList[randomNumber] === "#008080" ||
-    colorsList[randomNumber] === "#00ff7f" ||
-    colorsList[randomNumber] === "#333333" ||
-    colorsList[randomNumber] === "#101010"
-  ) {
-    divContainer.style.color = "white";
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hexs[getRandomNumber()];
   }
-
-  // We Could SIMPLIFIY it like this cause we choosed all elements in the list (array)
-  //   if (colorsList[randomNumber]) divContainer.style.color = "white";
+  document.body.style.background = hexColor;
+  colorPTag.textContent = hexColor;
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hexs.length);
+}
